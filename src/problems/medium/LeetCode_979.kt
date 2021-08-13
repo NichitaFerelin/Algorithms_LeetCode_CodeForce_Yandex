e8ab330f75460fbd9bb080915a9bf26d1ce3e780
NikitaFerelin <lizyh112@gmail.com>
2021-08-13 13:55:52 +0300

    979, 1380, 1387, 1415

---
+++ src/problems/medium/LeetCode_979.kt
@@ -0,0 +1,28 @@
+package problems.medium
+
+import kotlin.math.abs
+
+class LeetCode_979 {
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    private var steps = 0
+
+    fun distributeCoins(root: TreeNode?): Int {
+        postorder(root)
+        return steps
+    }
+
+    private fun postorder(node: TreeNode?): Int {
+        if (node == null) return 0
+
+        var coin = postorder(node.left) + postorder(node.right)
+
+        coin += if (node.`val` == 0) -1 else node.`val` - 1
+        steps += abs(coin)
+        return coin
+    }
+}
