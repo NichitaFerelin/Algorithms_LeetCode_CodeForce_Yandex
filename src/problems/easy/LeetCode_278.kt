192b6403cabdd7720f9435a6643c6014cc48836b
NikitaFerelin <lizyh112@gmail.com>
2021-08-21 17:51:25 +0300

    Day 21

---
+++ src/problems/easy/LeetCode_278.kt
@@ -0,0 +1,19 @@
+package problems.easy
+
+class LeetCode_278 {
+
+    /*override*/ fun firstBadVersion(n: Int): Int {
+        var leftBorder = 0
+        var rightBorder = n
+
+        while (leftBorder <= rightBorder) {
+            val middleVersion = leftBorder + (rightBorder - leftBorder) / 2
+
+            /*if (isBadVersion(middleVersion)) {
+                rightBorder = middleVersion - 1
+            } else leftBorder = middleVersion + 1*/
+        }
+
+        return rightBorder + 1
+    }
+}
