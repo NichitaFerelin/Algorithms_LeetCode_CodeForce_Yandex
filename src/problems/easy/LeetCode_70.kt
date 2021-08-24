8c300b5f47220cfe38e488c56b5d8c784d03deef
NikitaFerelin <lizyh112@gmail.com>
2021-08-24 14:09:44 +0300

    Day 24

---
+++ src/problems/easy/LeetCode_70.kt
@@ -0,0 +1,18 @@
+package problems.easy
+
+class LeetCode_70 {
+
+    fun climbStairs(n: Int): Int {
+        if (n == 1) return 1
+        if (n == 2) return 2
+
+        val dp = IntArray(n)
+        dp[0] = 1
+        dp[1] = 2
+
+        for (index in 2 until n) {
+            dp[index] = dp[index - 1] + dp[index - 2]
+        }
+        return dp[dp.lastIndex]
+    }
+}
---
+++ src/problems/medium/LeetCode_720.kt
@@ -0,0 +1,39 @@
+package problems.medium
+
+class LeetCode_720 {
+
+    fun longestWord(words: Array<String>): String {
+        val sortedWords = words.sortedByDescending { it.length }
+        val wordsBuildContainer = HashMap<String, Unit>()
+        sortedWords.forEach { wordsBuildContainer[it] = Unit }
+
+        val allResults = mutableListOf<String>()
+
+        var longestWordCounter = 0
+        sortedWords.forEach { currentWord ->
+            var stepCounter = 1
+            var stepWord = currentWord
+            while (stepWord.length != 1) {
+                if (wordsBuildContainer[stepWord] == null) {
+                    break
+                } else {
+                    stepCounter++
+                    stepWord = stepWord.substring(0, stepWord.lastIndex)
+                }
+            }
+
+            if (wordsBuildContainer[stepWord] != null) {
+                if (longestWordCounter < stepCounter) {
+                    allResults.clear()
+                    longestWordCounter = stepCounter
+                    allResults.add(currentWord)
+                } else if (longestWordCounter == stepCounter) {
+                    allResults.add(currentWord)
+                }
+            }
+        }
+
+        allResults.sort()
+        return allResults.getOrNull(0) ?: ""
+    }
+}
