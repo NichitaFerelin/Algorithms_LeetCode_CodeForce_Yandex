8130d045760e769f717abe881da1c232c14d295a
NikitaFerelin <lizyh112@gmail.com>
2021-09-01 21:13:02 +0300

    Day 32

--- src/problems/yandex/algorithms_1_B/D.kt
+++ src/problems/yandex/algorithms_1_B/D.kt
@@ -1,8 +1,13 @@
 package problems.yandex.algorithms_1_B
 
+import readFromConsole
+
 class D {
 
     fun alg() {
-        // TODO
+        val data = readFromConsole()
+        val houses = data[1].split(" ")
+        val result = houses[houses.size / 2].toInt()
+        println(result)
     }
 }
